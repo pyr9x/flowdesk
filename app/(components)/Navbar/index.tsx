ee156833b263a753ef709b6ae6aed009784b00be
Ore <akytect8273@gmail.com>
2025-03-19 13:55:17 +0000

    Dark mode toggle button to Navbar

--- app/(components)/Navbar/index.tsx
+++ app/(components)/Navbar/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Search, Settings } from "lucide-react";
+import { Menu, Moon, Search, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import { useAppSelector, useAppDispatch } from "@/redux/store";
+import {
+  setIsDarkMode,
+  setIsSidebarCollapsed,
+} from "@/redux/state/sidebar-slice";
 
 const Navbar = () => {
   const dispatch = useAppDispatch();
@@ -14,6 +18,13 @@ const Navbar = () => {
     <div className="dark:bg-dark-secondary flex items-center justify-between bg-white px-4 py-3">
       {/* Search Bar */}
       <div className="flex items-center gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 dark:text-white" />
+          </button>
+        )}
         <div className="relative flex h-min w-[200px]">
           <Search className="absolute top-1/2 left-[4px] mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-black" />
           <input
@@ -26,9 +37,27 @@ const Navbar = () => {
 
       {/* Icons*/}
       <div className="flex items-center">
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+        >
+          {isDarkMode ? (
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+          ) : (
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          )}
+        </button>
         <Link
           href="/settings"
-          className="h-min w-min rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
+          className={
+            isDarkMode
+              ? `h-min w-min rounded p-2 dark:hover:bg-gray-700`
+              : `h-min w-min rounded p-2 hover:bg-gray-100`
+          }
         >
           <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
         </Link>
