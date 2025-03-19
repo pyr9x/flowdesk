6478b588d6d996766284366af64241a5eb88baa8
Ore <akytect8273@gmail.com>
2025-03-19 18:52:14 +0000

    update sidebar and navbar

--- app/(components)/Navbar/index.tsx
+++ app/(components)/Navbar/index.tsx
@@ -26,9 +26,9 @@ const Navbar = () => {
           </button>
         )}
         <div className="relative flex h-min w-[200px]">
-          <Search className="absolute top-1/2 left-[4px] mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
+          <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
-            className="w-full rounded-full border-none bg-gray-100 p-2 pl-8 text-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
+            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
             type="search"
             placeholder="Search..."
           />
